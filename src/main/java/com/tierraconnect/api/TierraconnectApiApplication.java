efd5c6f4c0764e1482319168b80266266bb43976
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-20 12:32:58 -0500

    chore(pom): test commit

--- src/main/java/com/tierraconnect/api/TierraconnectApiApplication.java
+++ src/main/java/com/tierraconnect/api/TierraconnectApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TierraconnectApiApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(TierraconnectApiApplication.class, args);
     }
